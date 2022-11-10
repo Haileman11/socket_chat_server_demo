d67f1c7badcd2d35d7e3e4cec5885d2017338dd0
Hailegabriel Mekonnen <haile.mak11@gmail.com>
2022-11-10 17:20:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const server = require('http').createServer()
+const io = require('socket.io')(server)
+
+io.on('connection', function (client) {
+
+  console.log('client connect...', client.id);
+
+  client.on('typing', function name(data) {
+    console.log(data);
+    io.emit('typing', data)
+  })
+
+  client.on('message', function name(data) {
+    console.log(data);
+    io.emit('message', data)
+  })
+
+  client.on('location', function name(data) {
+    console.log(data);
+    io.emit('location', data);
+  })
+  
+  client.on('driver_profile', function name(data) {
+    console.log(data);
+    io.emit('driver_profile', {
+      id: '1',
+      name: 'Haile',      
+      phoneNumber: 'phoneNumber',
+      isAvailable: true,
+      status: "true",
+    });
+  })
+
+  client.emit('driver_profile',"Hello"
+    // {
+    // 'id': '1',
+    // 'name': 'Haile',      
+    // 'phoneNumber': 'phoneNumber',
+    // 'isAvailable': true,
+    // 'status': "true",
+    // }
+  );
+
+  client.on('connect', function () {
+  })
+
+  
+
+  client.on('disconnect', function () {
+    console.log('client disconnect...', client.id)
+    // handleDisconnect()
+  })
+
+  client.on('error', function (err) {
+    console.log('received error from client:', client.id)
+    console.log(err)
+  })
+})
+
+var server_port = process.env.PORT || 3000;
+server.listen(server_port, function (err) {
+  if (err) throw err
+  console.log('Listening on port %d', server_port);
+});
